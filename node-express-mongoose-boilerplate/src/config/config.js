93c122b4086f9b691bea6df1f0dd2993242f6273
shravan20 <mrshravankumarb@gmail.com>
2021-10-03 17:41:04 +0530

    [Added prettier and eslinter config ] | feat | updated style-guide, added environment setup

---
+++ node-express-mongoose-boilerplate/src/config/config.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import Joi from 'joi';
+
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const envVarsSchema = Joi.object().keys({
+    PORT: Joi.number().default(3000),
+    NODE_ENV: Joi.string().valid('production', 'development', 'test').required()
+}).unknown();
+
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+
+
+module.exports = {
+    port: envVars.PORT,
+    env: envVars.NODE_ENV
+}
