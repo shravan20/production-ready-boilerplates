3fa13c1846d085cd5f61b8c0830ab5462b79aaaa
shravan20 <mrshravankumarb@gmail.com>
2021-10-03 20:08:14 +0530

    [ prettier fix ] | style | applied prettier

--- node-express-mongoose-boilerplate/src/app.js
+++ node-express-mongoose-boilerplate/src/app.js
@@ -8,6 +8,6 @@ app.use(express.json());
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (request,response, next)=>response.send("Testing"));
+app.get('/', (request, response, next) => response.send('Testing'));
 
 module.exports = app;
