50e5b47bddb42dc4a37d0578b3c741548c53b109
shravan20 <mrshravankumarb@gmail.com>
2021-10-03 20:58:03 +0530

    [ node-mongoose-boilerplate] | feat | enabled cors

--- node-express-mongoose-boilerplate/src/app.js
+++ node-express-mongoose-boilerplate/src/app.js
@@ -13,4 +13,9 @@ app.use('/v1', routes);
 
 app.get('/', (request, response, next) => response.send('Testing'));
 
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
 module.exports = app;
