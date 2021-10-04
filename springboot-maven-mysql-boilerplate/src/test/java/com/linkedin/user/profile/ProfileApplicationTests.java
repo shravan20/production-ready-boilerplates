1aa378ad3d536f0f0d397db05c4e553e15a1a83d
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2021-10-04 17:16:47 +0530

    Initial Project Structure

---
+++ springboot-maven-mysql-boilerplate/src/test/java/com/linkedin/user/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.user.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
